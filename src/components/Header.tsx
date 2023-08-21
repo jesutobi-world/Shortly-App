a7d0f4175e87205ea6b8cd962a7ccf8f820ce33a
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2023-08-21 09:41:46 +0100

    toggle functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,23 @@
-
+import { useState } from "react";
 
 export const Header = () => {
+  const [isNavVisible, setIsNavVisible] = useState(window.innerWidth >=1000)
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
   return (
     <header>
     <section>
       <div className="logo">
         <p>Shortly</p>
       </div>
-      <div className="hamburger">
+      <div className="hamburger" 
+      onClick={toggleNav}
+      >
         <i className="fa-solid fa-bars"></i>
       </div>
     </section>
-    <nav>
+    <nav style={{ display: isNavVisible ? "block" : "none" }}>
       <section className="border">
         <a href="#">Features</a>
         <a href="#">Pricing</a>
